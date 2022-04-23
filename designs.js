799d2453f4582a6db6b4056ce9d498a2defe3763
queen-x-vee <valentinabuoro4@gmail.com>
2022-04-24 00:25:23 +0100

    update

--- designs.js
+++ designs.js
@@ -1,10 +1,40 @@
 // Select color input
 // Select size input
 
+//create variables for height, width and color
+var height, width, color;
+// Add an event listener to listen for submit
+let form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    height = document.querySelector("#inputHeight").value;
+    width = document.querySelector("#inputWidth").value;
+    makeGrid(height,width)
+    
+
+})
+
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function makeGrid(x,y) {
+  document.querySelector('#pixelCanvas').innerHTML = '';
+ // Your code goes here!
+   for(var i = 1; i<=x; i++){
+       document.querySelector('#pixelCanvas').innerHTML += `<tr id='table-${i}'></tr>`
+       for(var j = 1; j<=y; j++)
+       document.querySelector(`#table-${i}`).innerHTML += `<td class='cell'></td>`
+   }
+   
+   
 
-// Your code goes here!
-
+    const cells = document.getElementsByClassName('cell');
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].addEventListener("click", function (event) {
+            let clickedCell = event.target;
+            clickedCell.style.backgroundColor = colorPicker.value;
+        });
+    }
+   
 }
+
+
